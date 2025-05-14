9676f0e622ac034ae141b2f27f8d45b9dc6c750d
liangbx <liangbx361@163.com>
2025-05-14 11:32:57 +0800

    fix: 移除无效引用

--- android/src/main/java/com/umeng/message/UmengPushSdkPlugin.java
+++ android/src/main/java/com/umeng/message/UmengPushSdkPlugin.java
@@ -32,7 +32,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * 推送Flutter插件类
